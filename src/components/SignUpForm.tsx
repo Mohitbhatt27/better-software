7b99caa812f5b7ff3a8645be81b32c672db32c19
Mohit <mohit.peak@gmail.com>
2024-12-08 18:53:57 +0530

    added password strength feature

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,10 +1,11 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
 import "../App.css";
 
 const SignUpForm: React.FC = () => {
   const initialValues = { email: "", password: "" };
+  const [passwordStrength, setPasswordStrength] = useState("");
 
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email format").required("Required"),
@@ -18,6 +19,20 @@ const SignUpForm: React.FC = () => {
     console.log(values);
   };
 
+  const assessPasswordStrength = (password: string) => {
+    let strength = "Weak";
+    if (
+      password.length > 8 &&
+      /[A-Z]/.test(password) &&
+      /[\d]/.test(password)
+    ) {
+      strength = "Strong";
+    } else if (password.length > 5) {
+      strength = "Medium";
+    }
+    setPasswordStrength(strength);
+  };
+
   return (
     <div className="container">
       <h2>Sign Up</h2>
@@ -26,19 +41,29 @@ const SignUpForm: React.FC = () => {
         validationSchema={validationSchema}
         onSubmit={onSubmit}
       >
-        <Form>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field name="email" type="email" />
-            <ErrorMessage name="email" component="div" className="error" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Field name="password" type="password" />
-            <ErrorMessage name="password" component="div" className="error" />
-          </div>
-          <button type="submit">Sign Up</button>
-        </Form>
+        {({ setFieldValue }) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field name="email" type="email" />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field
+                name="password"
+                type="password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setFieldValue("password", e.target.value);
+                  assessPasswordStrength(e.target.value);
+                }}
+              />
+              <ErrorMessage name="password" component="div" className="error" />
+              <div className="password-strength">{passwordStrength}</div>
+            </div>
+            <button type="submit">Sign Up</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
